6b4beeac6695addfd47497e9f486213e4642e558
thecupangin <93725433+thecupangin@users.noreply.github.com>
2022-04-02 03:42:54 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,19 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# change ssid
+sed -i "s/OpenWrt/CupangOs/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i "s/disabled=1/disabled=0/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# change hostname
+sed -i "s/OpenWrt/CupangOs/g" package/base-files/files/bin/config_generate
+
+# change timezone
+sed -i "s/UTC/WIB-7/g" package/base-files/files/bin/config_generate
+
+# change banner
+rm -rf ./package/base-files/files/etc/banner
+svn export https://github.com/thecupangin/CupangOs-LEDE/trunk/amlogic-s9xxx/common-files/files/etc/banner package/base-files/files/etc/banner
+
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
